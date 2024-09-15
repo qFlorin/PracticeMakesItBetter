8b38ba72997c8607990200cfd1cdef83a3cb55cd
Strix <puiaflorin94@gmail.com>
2024-09-15 13:09:02 +0300

    More practice

--- src/app/component-basics/parent/parent.component.ts
+++ src/app/component-basics/parent/parent.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import {
+  afterNextRender,
+  afterRender,
+  AfterRenderOptions,
+  AfterViewInit,
+  Component,
+  ElementRef,
+  inject,
+} from '@angular/core';
 import { ChildComponent } from '../child/child.component';
-import { CurrencyPipe, JsonPipe } from '@angular/common';
+import {
+  CurrencyPipe,
+  JsonPipe,
+  NgComponentOutlet,
+  NgTemplateOutlet,
+} from '@angular/common';
+import { SiblingComponent } from '../sibling/sibling.component';
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.scss'],
   standalone: true,
-  imports: [ChildComponent, CurrencyPipe, JsonPipe],
+  imports: [
+    ChildComponent,
+    CurrencyPipe,
+    JsonPipe,
+    NgComponentOutlet,
+    SiblingComponent,
+    NgTemplateOutlet,
+  ],
 })
-export class ParentComponent {}
+export class ParentComponent {
+  sibling = SiblingComponent;
+  elRef = inject(ElementRef);
+
+  constructor() {
+    afterNextRender(() => {
+      this.elRef.nativeElement.querySelector('#currency').style.color = 'red';
+    });
+  }
+}
