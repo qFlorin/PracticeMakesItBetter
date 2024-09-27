6735f8b6bd73b7e7e3092607bcbc9b086b6d59d1
Strix <puiaflorin94@gmail.com>
2024-09-27 13:55:55 +0300

    First table look

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,3 +1,4 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,9 +6,27 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
   standalone: true,
+  imports: [NgFor, NgIf],
 })
 export class TableComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  tableColumns: string[] = ['ID', 'Name', 'Age', 'Email'];
+  tableRows: { [key: string]: any }[] = [
+    { ID: 1, Name: 'John Doe', Age: 25, Email: 'john.doe@example.com' },
+    { ID: 2, Name: 'Jane Smith', Age: 30, Email: 'jane.smith@example.com' },
+    { ID: 3, Name: 'Sam Johnson', Age: 22, Email: 'sam.johnson@example.com' },
+  ];
+
+  getTableData() {
+    return this.tableRows.map((row) => {
+      let rowData: { [key: string]: any } = {};
+      this.tableColumns.forEach((column) => {
+        rowData[column] = row[column];
+      });
+      return rowData;
+    });
+  }
 }
