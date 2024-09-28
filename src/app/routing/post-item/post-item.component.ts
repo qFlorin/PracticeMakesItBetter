d5fa56f381b2616d609c3d8104d384c550735344
Strix <puiaflorin94@gmail.com>
2024-09-28 11:08:26 +0300

    Beginning of routing

---
+++ src/app/routing/post-item/post-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-item',
+  templateUrl: './post-item.component.html',
+  styleUrls: ['./post-item.component.scss'],
+  standalone: true,
+  imports: [],
+})
+export class PostItemComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ src/app/routing/posts/posts.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.scss'],
+  standalone: true,
+  imports: [],
+})
+export class PostsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
