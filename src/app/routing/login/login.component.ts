d788a509765cad292e36bffb487b87a422858aa3
Strix <puiaflorin94@gmail.com>
2024-09-30 23:16:37 +0300

    Remove unnecesary

--- src/app/routing/login/login.component.ts
+++ src/app/routing/login/login.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-
+import { AuthService } from './../services/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,6 +10,8 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   imports: [ReactiveFormsModule],
 })
 export class LoginComponent implements OnInit {
+  private authService = inject(AuthService);
+  private router = inject(Router);
   public loginForm = new FormGroup({
     username: new FormControl('user1'),
     password: new FormControl('pass1'),
@@ -16,4 +19,20 @@ export class LoginComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  login() {
+    const username = this.loginForm.get('username')?.value;
+    const password = this.loginForm.get('password')?.value;
+
+    if (username && password) {
+      const user = this.authService.login(username, password);
+      if (user !== undefined) {
+        alert(user?.username + ' is logged in');
+        this.router.navigate(['/orders']);
+      } else {
+        console.error('Username or password is missing');
+        alert('Username or password is missing');
+      }
+    }
+  }
 }
--- src/app/routing/services/auth.service.ts
+++ src/app/routing/services/auth.service.ts
@@ -1,30 +1,26 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  isLogged = false;
+  isLogged = signal(false);
   userService = inject(UserService);
   constructor() {}
 
-  login(username: string, password: string): boolean {
+  login(
+    username: string,
+    password: string
+  ): { id: number; username: string; password: string } | undefined {
     const user = this.userService.users.find(
       (user) => user.username === username && user.password === password
     );
     if (user) {
-      this.isLogged = true;
-      return true;
+      this.isLogged.set(true);
+    } else {
+      this.isLogged.set(false);
     }
-    return false;
-  }
-
-  logout(): void {
-    this.isLogged = false;
-  }
-
-  isLoggedIn(): boolean {
-    return this.isLogged;
+    return user;
   }
 }
