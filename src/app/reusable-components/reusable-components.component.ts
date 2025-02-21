92f72214a0a6a66fb842e8e042e526b1b8d6c349
Strix <puiaflorin94@gmail.com>
2025-02-21 15:48:37 +0200

    Learn and earn part ...

---
+++ src/app/reusable-components/reusable-components.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reusable-components',
+  templateUrl: './reusable-components.component.html',
+  styleUrls: ['./reusable-components.component.scss'],
+})
+export class ReusableComponentsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
